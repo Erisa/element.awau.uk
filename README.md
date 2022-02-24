e71a893bad6bffb814687f530475544307a3ed1a
Erisa A <erisa@riseup.net>
2022-02-24 00:03:40 +0000

    code push

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # element.awau.uk
 Element Element Element Element Element Element Element Element Element 
+
+## Usage
+
+- Edit `config.json` to fit needs.
+- Create Cloudflare Pages project with repo.
+- Set build command as `./build.sh` and output dir as `out`
+- ??? Build and win.
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+# configure here
+ELEMENT_VERSION=v1.10.4
+# end configure here
+
+wget -O element.tar.gz https://github.com/vector-im/element-web/releases/download/$ELEMENT_VERSION/element-$ELEMENT_VERSION.tar.gz
+
+tar -xvf element.tar.gz
+mv element-$ELEMENT_VERSION out
+cp config.json out/config.json
+
+# ./out is now ready.

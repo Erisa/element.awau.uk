9a251bddb71d7573c99b1cf419e0ee2a63b7d7e3
Spotlight <spotlight@joscomputing.space>
2022-02-28 11:49:11 -0600

    Update Element to v1.10.5
    See changelog here: https://github.com/vector-im/element-web/releases/tag/v1.10.5

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -x
 
 # configure here
-ELEMENT_VERSION=v1.10.4
+ELEMENT_VERSION=v1.10.5
 # end configure here
 
 wget -O element.tar.gz https://github.com/vector-im/element-web/releases/download/$ELEMENT_VERSION/element-$ELEMENT_VERSION.tar.gz

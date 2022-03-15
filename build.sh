6e6dbfad86724180516055f1e264d7df0f89c8cc
Erisa A <erisa@erisa.uk>
2022-03-15 15:19:05 +0000

    Update Element to v1.10.7
    See changelog here: https://github.com/vector-im/element-web/releases/tag/v1.10.7

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -x
 
 # configure here
-ELEMENT_VERSION=v1.10.6
+ELEMENT_VERSION=v1.10.7
 # end configure here
 
 wget -O element.tar.gz https://github.com/vector-im/element-web/releases/download/$ELEMENT_VERSION/element-$ELEMENT_VERSION.tar.gz

5fc519e0b4c56ea8f8d986d154c50ef3076e4005
Spotlight <spotlight@joscomputing.space>
2024-03-28 12:07:41 -0500

    Update Element to v1.11.63
    https://github.com/element-hq/element-web/releases/tag/v1.11.63

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -x
 
 # configure here
-ELEMENT_VERSION=v1.11.62
+ELEMENT_VERSION=v1.11.63
 # end configure here
 
 curl -L -o element.tar.gz https://github.com/vector-im/element-web/releases/download/$ELEMENT_VERSION/element-$ELEMENT_VERSION.tar.gz

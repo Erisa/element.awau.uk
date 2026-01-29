4ec54871f12cfca766bc04cc4a5438456e876aba
Spotlight <spotlight@joscomputing.space>
2026-01-29 02:06:44 -0500

    Update Element to v1.12.8
    https://github.com/element-hq/element-web/releases/tag/v1.12.8

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -x
 
 # configure here
-ELEMENT_VERSION=v1.12.8
+ELEMENT_VERSION=v1.12.9
 # end configure here
 
 curl -L -o element.tar.gz https://github.com/element-hq/element-web/releases/download/$ELEMENT_VERSION/element-$ELEMENT_VERSION.tar.gz
